436dd6d2662d6b17b145112b7b68cf8fb97d5c34
Shoxruz <forpk432@gmail.com>
2024-01-27 18:58:08 +0500

    adding_travel_and_activity_pages

--- src/components/moreInfo.tsx
+++ src/components/moreInfo.tsx
@@ -9,14 +9,22 @@ import hotel_one from '../assets/hotel-one.jpg'
 import hotel_two from '../assets/hotel-two.jpg'
 import hotel_three from '../assets/hotel-three.jpg'
 import hotel_four from '../assets/hotel-four.jpg'
+import travel_one from '../assets/travel-one.jpg'
+import travel_two from '../assets/travel-two.jpg'
+
+import act_one from '../assets/act-one.jpg'
+import act_two from '../assets/act-two.jpg'
+import act_three from '../assets/act-three.jpg'
+import act_four from '../assets/act-four.jpg'
+
 import Image from 'next/image'
 import CustomImage from './customImage'
 
 import ReactStars from 'react-stars'
 
 
-import { FaLocationDot } from "react-icons/fa6";
-import { FaLongArrowAltRight } from "react-icons/fa";
+import { FaLocationDot, FaRegComment } from "react-icons/fa6";
+import { FaLongArrowAltRight, FaRegCalendar, FaRegUser } from "react-icons/fa";
 
 import Link from 'next/link'
 
@@ -82,6 +90,44 @@ export default function MoreInfo() {
             rate: 3
         },
     ]
+
+    const travelData = [
+        {
+            img: travel_one,
+            title: 'East Village Ice Cream Crawl',
+            desc: "We will stop at five different world-class ice cream shops on this 1.5 mile 1.5 hour tour. At each ice cream store we'll explore the story behind the business and see what makes the ice cream unique as you savor every…",
+            date: 'Today',
+            writer: 'Maria Philips',
+            comment: 2
+        },
+        {
+            img: travel_two,
+            title: 'Brooklyn Bridge cinematic photo walk',
+            desc: 'This experience takes place at the Brooklyn Bridge Park and Brooklyn Bridge, but I’m always open to capture clients at different locations upon request for an additional charge. ',
+            date: 'Today',
+            writer: 'James Calzoni',
+            comment: 17
+        }
+    ]
+
+    const activitiesData = [
+        {
+            img: act_one,
+            title: 'Sailing'
+        },
+        {
+            img: act_two,
+            title: 'Climbing'
+        },
+        {
+            img: act_three,
+            title: 'Skiing'
+        },
+        {
+            img: act_four,
+            title: 'Hiking'
+        },
+    ]
     return (
         <>
             <div>
@@ -90,7 +136,7 @@ export default function MoreInfo() {
                     {
                         pop_detailed.map((item, index) => (
                             <div key={index} >
-                                <div className="image-container relative h-[270px] lg:h-[220px]">
+                                <div className="image-container relative h-[270px] lg:h-[220px] xl:h-[350px]">
                                     <CustomImage item={item} />
                                 </div>
                                 <h2 className='text-[#161414] fpmt-[600] text-[16px] mt-[8px] leading-none'>{item.title}</h2>
@@ -105,7 +151,7 @@ export default function MoreInfo() {
                     <h1 className='text-[#161414] text-[20px] lg:text-[32px] font-[600] pt-[40px] lg:pt-[80px] pb-[40px] lg:pb-[60px]'>Hotels and Restaurants</h1>
                     <Link href={'/'} className='flex justify-center items-center gap-[5px] bg-[#F6F4FF] p-[10px] rounded-[12px]'>
                         <span className='text-[13px] md:text-[16px] text-[#5243C2]'>View all</span>
-                        <FaLongArrowAltRight color='#5243C2'/>
+                        <FaLongArrowAltRight color='#5243C2' />
                     </Link>
                 </div>
                 <div className="hotels-container grid grid-cols-1 gap-y-[20px] md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-[30px]">
@@ -123,6 +169,67 @@ export default function MoreInfo() {
                                     </div>
                                     <ReactStars value={item.rate} edit={false} size={16} />
                                 </div>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+            <div className="travel-section">
+                <div className="starter flex justify-between items-center">
+                    <h1 className='text-[#161414] text-[20px] lg:text-[32px] font-[600] pt-[40px] lg:pt-[80px] pb-[40px] lg:pb-[60px]'>Travel Tips and Advice</h1>
+                    <Link href={'/'} className='flex justify-center items-center gap-[5px] bg-[#F6F4FF] p-[10px] rounded-[12px]'>
+                        <span className='text-[13px] md:text-[16px] text-[#5243C2]'>View all</span>
+                        <FaLongArrowAltRight color='#5243C2' />
+                    </Link>
+                </div>
+                <div className="travel-container flex justify-between flex-col xl:flex-row gap-[30px]">
+                    {
+                        travelData.map((item, index) => (
+                            <div key={index} className='flex flex-col justify-center md:flex-row gap-[15px] md:gap-[30px]'>
+                                <div className="image-container relative h-[320px] lg:h-[320px] w-[100%] md:w-[47%] xl:h-[400px]">
+                                    <CustomImage item={item} />
+                                </div>
+                                <div className='w-[100%] md:w-[47%] md:min-h-[100%] flex justify-between flex-col pb-[10px]'>
+                                    <div>
+                                        <h1 className='text-[#161414] text-[20px] font-[600] mb-[16px]'>{item.title}</h1>
+                                        <p className='text-[#5B5B5B] text-[16px]'>{item.desc}</p>
+                                    </div>
+                                    <div className="info-part flex justify-between items-center">
+                                        <div className='flex items-center gap-[4px]'>
+                                            <FaRegCalendar color='#878787' size={16} />
+                                            <span className='text-[#878787] text-[12px]'>{item.date}</span>
+                                        </div>
+                                        <div className='flex items-center gap-[4px]'>
+                                            <FaRegUser color='#878787' size={16} />
+                                            <span className='text-[#878787] text-[12px]'>{item.writer}</span>
+                                        </div>
+                                        <div className='flex items-center gap-[4px]'>
+                                            <FaRegComment color='#878787' size={16} />
+                                            <span className='text-[#878787] text-[12px]'>{item.comment}</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+            <div className='activities-section'>
+                <div className="starter flex justify-between items-center">
+                    <h1 className='text-[#161414] text-[20px] lg:text-[32px] font-[600] pt-[40px] lg:pt-[80px] pb-[40px] lg:pb-[60px]'>Travel Tips and Advice</h1>
+                    <Link href={'/'} className='flex justify-center items-center gap-[5px] bg-[#F6F4FF] p-[10px] rounded-[12px]'>
+                        <span className='text-[13px] md:text-[16px] text-[#5243C2]'>View all</span>
+                        <FaLongArrowAltRight color='#5243C2' />
+                    </Link>
+                </div>
+                <div className="act-container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-x-[32px] gap-y-[20px]">
+                    {
+                        activitiesData.map((item, index) => (
+                            <div key={index}>
+                                <div className="image-container relative h-[320px] lg:h-[270px] w-[100%] md:w-[100%] xl:h-[400px]">
+                                    <CustomImage item={item} />
+                                </div>
+                                <h2 className='text-[#161414] text-[16px] font-[500] mt-[16px]'>{item.title}</h2>
                             </div>
                         ))
                     }
